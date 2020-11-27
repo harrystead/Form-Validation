cb41fed50cda33b16bf066b951f291de66cc7320
Harry Stead <harrystead9@gmail.com>
2020-11-27 11:40:46 +0000

    if username has spaces, return error

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,54 @@
 //entire orm submitted on one click
 //function to run five problems
 
-function validateForm() {
-  var nameInput = $("#name-input").val().toLowerCase();
-  var containsDigit = /\d/;
-  var containsCap = /[A-Z]/;
-  var invalid = [];
+$( document ).ready(function (){
+    function validateNameForm() {
+        var nameInput = $("#name-input").val().toLowerCase();
+        var containsDigit = /\d/;
+        var invalid = [];
+      
+        if (nameInput.length === 0 || nameInput.length < 6 || nameInput.length > 12) {
+          console.log(false);
+      
+          invalid.push("error: username must be between 6 and 12 letters long");
+          console.log(invalid);
+        } else {
+          console.log(true);
+        }
+      
+        if (containsDigit.test(nameInput)) {
+          console.log(true);
+        } else {
+          invalid.push("error: password must contain a number (1-10)");
+          console.log(invalid);
+        }
 
-  if (nameInput.length === 0 || nameInput.length < 6 || nameInput.length > 12) {
-    console.log(false);
+        if(nameInput.indexOf(' ') > 0) {
+            console.log(true);
+            invalid.push("error: password must not contain spaces")
+            console.log(invalid);
+        }
+        else{
+            console.log(false);
+        }
 
-    invalid.push(
-      "error: username must be between 6 and 12 letters long and must also include a number"
-    );
-  }
-  else{
-      console.log(true);
-  }
+      
+        $("#error-list").append(invalid);
+      }
+      
+      function validatePhoneForm() {
+      
+      }
+      
+      $("#submit-btn").on("click", function (e) {
+        e.preventDefault();
+      
+        validateNameForm();
+        validatePhoneForm();
+      });
 
-  if (containsDigit.test(nameInput)) {
-    console.log(true);
-  }
-  else {
-    invalid.push(
-        "error: password must contain a number (1-10)"
-        );
-        console.log(invalid);
-  }
 
-  $("#error-div").append(invalid);
-}
 
-$("#submit-btn").on("click", function (e) {
-  e.preventDefault();
-
-  validateForm();
 });
+
+
